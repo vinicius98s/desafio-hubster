4aec0f4a343cdc61f7d9daab5fea7b5e294a92d5
Vinicius <vinicius.sales@hubster.com.br>
2019-10-09 19:19:10 -0300

    edit API url

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,15 +1,19 @@
-import { showLoading, hideLoading } from 'react-redux-loading';
-import { receiveData } from './data';
+import { showLoading, hideLoading } from "react-redux-loading";
+import { receiveData } from "./data";
 
 export function handleInitialData() {
-    return (dispatch) => {
-        dispatch(showLoading())
+  return dispatch => {
+    dispatch(showLoading());
 
-        return fetch('http://5c90795d8447f30014cb8438.mockapi.io/api/v1/catalog/products')
-            .then(res => res.json())
-            .then((data) => {
-                dispatch(receiveData({ products: data[0].products, categories: data[0].categories }))
-            })
-            .then(dispatch(hideLoading()))
-    }
+    return fetch(
+      "http://5d556e6936ad770014cce06d.mockapi.io/api/v1/catalog/products"
+    )
+      .then(res => res.json())
+      .then(data => {
+        dispatch(
+          receiveData({ products: data.products, categories: data.categories })
+        );
+      })
+      .then(dispatch(hideLoading()));
+  };
 }
